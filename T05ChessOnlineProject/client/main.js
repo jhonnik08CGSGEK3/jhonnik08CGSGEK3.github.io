91aec6ee0b37576c6e203f853ed3d5582d4e9e80
CGSG-EK3-SUMMERPRACTICE <106954475+jhonnik08CGSGEK3@users.noreply.github.com>
2023-06-20 18:10:28 +0300

    Add files via upload

--- T05ChessOnlineProject/client/main.js
+++ T05ChessOnlineProject/client/main.js
@@ -1,29 +1,71 @@
 import { io } from "socket.io-client";
 import { makeMove } from "../dist/chessLogic.js"
 
+function moveAudio() {
+  var audio = new Audio();
+  audio.src = 'bin/Move.mp3';
+  audio.autoplay = true;
+}
+function captureAudio() {
+  var audio = new Audio();
+  audio.src = 'bin/Capture.mp3';
+  audio.autoplay = true;
+}
+function errorAudio() {
+  var audio = new Audio();
+  audio.src = 'bin/Error.mp3';
+  audio.autoplay = true;
+}
+function notifyAudio() {
+  var audio = new Audio();
+  audio.src = 'bin/GenericNotify.mp3';
+  audio.autoplay = true;
+}
+
+let inGame = false, inMM = false;
+
 async function main() {
   const socket = io();
 
   // client-side
   socket.on("connect", () => {
     console.log(socket.id); // x8WIv7-mJelg7on_ALbx
+    //document.getElementById("whitePlayer").innerText = "White: " + socket.id + " (You)";
     socket.on("MessageFromServer", function (msg) {
-        let res;
+        let res, capture;
         let textDoc = document.getElementById("history");
+        let chatHist = document.getElementById("historyChat");
         console.log(msg);
 
         if (msg.length === 5 && msg[2] == ' ') {
-            let tableCell1 = document.getElementById(msg[0] + msg[1]);
-            let tableCell2 = document.getElementById(msg[3] + msg[4]);
+            let tableCell1 = document.getElementById(msg[0].toUpperCase() + msg[1]);
+            let tableCell2 = document.getElementById(msg[3].toUpperCase() + msg[4]);
+            if (tableCell2 !== null)
+                if (tableCell2.innerHTML !== "")
+                    capture = true;
             res = makeMove(tableCell1, tableCell2);
-            //tableCell1.sty
-            textDoc.value += "\n" + res;
+            if (res == -1) {
+                chatHist.value += "\n" + msg;
+                chatHist.scrollTop = chatHist.scrollHeight;   
+            }
+            else if (res == 0) {
+                textDoc.value += "\n" + tableCell1.id + " " + tableCell2.id + ": Invalid move!";
+                errorAudio();
+            }
+            else if (res == 1) {
+                textDoc.value += "\n" + current_move + ". " + tableCell1.id + " " + tableCell2.id;
+                // Init sounds
+                if (capture) captureAudio();
+                else moveAudio();
+            }
         }
         else {
-            textDoc.value += "\n" + msg;
+            chatHist.value += "\n" + msg;
+            chatHist.scrollTop = chatHist.scrollHeight; 
         }
         
         textDoc.scrollTop = textDoc.scrollHeight;
+        //else return start.id + " " + finish.id + ": " + "Invalid move!";
     });
   });
 
@@ -32,7 +74,7 @@ async function main() {
   });
 
   document.getElementById("sentMsg").onkeyup = (ev) => {
-    if (ev.code === "Enter") {
+    if (ev.code === "Enter" && inGame) {
       const value = document.getElementById("sentMsg").value;
       console.log(value);
       document.getElementById("sentMsg").value = "";
@@ -40,6 +82,48 @@ async function main() {
       socket.emit("MessageToServer", value);
     }
   };
+
+  document.getElementById("joinButton").addEventListener("mousedown", () => {
+    if (!inGame) {
+      if (!inMM) socket.emit("mmJoinRequest");
+      else socket.emit("mmLeaveRequest");
+    }
+      // document.getElementById("whitePlayer").innerText = `White: ${socket.id} (You)`;
+  });
+
+  socket.on("joinedMM", () => {
+    inMM = true;
+    document.getElementById("joinButton").value = "Leave MM queue";
+  });
+
+  socket.on("leftMM", () => {
+    inMM = false;
+    document.getElementById("joinButton").value = "Join MM queue";
+  });
+
+  socket.on("startGame", (blackID) => {
+    inMM = false;
+    inGame = true;
+    document.getElementById("joinButton").value = "Leave game";
+    document.getElementById("whitePlayer").value = `White: ${socket.id}`;
+    document.getElementById("blackPlayer").value = `Black: ${blackID}`;
+  });
+  // document.getElementById("thePrompt").innerHTML = "You joined the game!";
+  //           document.getElementById("enemyBoardName").innerHTML = `${enemyID} (Enemy)`;
+  //           document.getElementById("theButton").value = "Concede";
+  //           (inMM = false),
+  //           (inGame = true);
+
+//   document.getElementById("theButton").addEventListener("mousedown", ()=>{
+//     if (!inGame) {
+//         if (!inMM)
+//             socket.emit("matchmakeRequest");
+//         else
+//             socket.emit("leaveMMrequest");
+//     } else
+//         socket.emit("concedeRequest");
+// }
+// );
 }
 
 window.addEventListener("load", (event) => {
--- T05ChessOnlineProject/server/server.js
+++ T05ChessOnlineProject/server/server.js
@@ -12,17 +12,17 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 const clients = [];
+const matchmaking = [];
+const rooms = [];
 
 io.on("connection", (socket) => {
   clients.push(socket);
+  //var room = uuid.v4();
   console.log(`Client connected with id: ${socket.id}`);
   socket.on("MessageToServer", (msg) => {
     const replyMsg = `${msg}`;
     console.log(replyMsg);
     for (client of clients) {
-      // if (client === socket) {
-      //   continue;
-      // }
       client.emit("MessageFromServer", replyMsg);
     }
   });
@@ -33,8 +33,30 @@ io.on("connection", (socket) => {
       clients.splice(index, 1);
     }
   });
+  socket.on("mmJoinRequest", () => {
+    console.log(`MATCHMAKING: Client joined MatchMaking with id: ${socket.id}`);
+    socket.emit("joinedMM");
+    matchmaking.push(socket);
+    if (matchmaking.length >= 2) {
+      let socket1 = matchmaking[0], socket2 = matchmaking[1];
+      socket1.emit("startGame", socket2.id)
+      socket2.emit("startGame", socket1.id)
+      const index1 = matchmaking.indexOf(matchmaking[0]);
+      if (index1 > -1) {
+        matchmaking.splice(index1, 2);
+      }
+    }
+  });
+  socket.on("mmLeaveRequest", () => {
+    console.log(`MATCHMAKING: Client LEFT MatchMaking with id: ${socket.id}`);
+    socket.emit("leftMM");
+    const index = matchmaking.indexOf(socket);
+    if (index > -1) {
+      matchmaking.splice(index, 1);
+    }
+  });
 });
 
-server.listen(process.env.PORT || 3000, () => {
+server.listen(process.env.PORT || 1234, () => {
   console.log(`Server started on port ${server.address().port} :)`);
 });
